1a2d1b994963f657f785d83e4e37c4539bb012d1
Vladimir <benzilko@gmail.com>
2022-08-08 14:43:11 +0300

    add detect ip addres

--- myapp.php
+++ myapp.php
@@ -4,5 +4,18 @@
  </head>
  <body>
  <p>Hello World</p>
+ <?php
+  
+// PHP program to obtain IP address of
+// the server
+  
+// Creating a variable to store the
+// server address
+$ip_server = $_SERVER['SERVER_ADDR'];
+  
+// Printing the stored address
+echo "Server IP Address is: $ip_server";
+  
+?>
  </body>
 </html>
